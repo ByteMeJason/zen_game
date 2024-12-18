40b14d9248ae6b94de96ed803c116108b3530798
Jason <jlondono223@outlook.com>
2024-12-18 10:33:00 -0500

    updated to declare modules

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader, Write};
+mod filter;
+mod game;
+mod word_utils;
 
-fn main() {
-    let lock = false;
+use filter::filter_words;
+use game::run_game;
+
+fn main() -> std::io::Result<()> {
+    let lock = false; // Set to true to filter the words
+
     if lock {
         match filter_words() {
-            Ok(_) => println!("filtering completed"),
-            Err(e) => eprintln!("An error occured: {}", e),
+            Ok(_) => println!("Filtering completed"),
+            Err(e) => eprintln!("An error occurred: {}", e),
         }
     } else {
         println!("Skipping word filtering. Set lock to true to filter words.");
     }
-   
+
+    run_game()
 }
 
-fn filter_words() -> io::Result<()> {
-    let input_file = File::open("words_alpha.txt")?;
-    let reader = BufReader::new(input_file);
 
-    // create output files
-    let mut six_letter_file = File::create("six_letter_words.txt")?;
-    let mut word_bank_file = File::create("word_bank.txt")?;
 
-    for line in reader.lines() {
-        let word = line?.trim().to_lowercase();
-
-        match word.len() {
-            6 => writeln!(six_letter_file, "{}", word)?, // Write 6-letter words
-            3..=5 => writeln!(word_bank_file, "{}", word)?, // Write 3, 4, 5-letter words
-            _ => {} // skip all other lengthed words
-        }
-    }
-    println!("Words successfully filtered & saved!");
-    Ok(())
-}
